56b36f94643fa39c0b5babc98e1251aa4965549a
Darren <you@example.comdarren@kandekore.net>
2024-01-16 19:32:48 +0000

    Token Boxes

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined
+} from "@ant-design/icons";
+import tokenList from "../tokenList.json";
 
 function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
+  }
+  function changeAmount(e) {
+    setTokenOneAmount(e.target.value);
+  }
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  );
   return (
-    <div>Swap</div>
-  )
+    <div className="tradeBox">
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover
+          content={settings}
+          title="Settings"
+          trigger="click"
+          placement="bottomRight"
+        >
+          <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+      <div className="inputs">
+        <Input
+          placeholder="0"
+          value={tokenOneAmount}
+          onChange={changeAmount}
+          // disabled={!prices}
+        />
+        <Input
+          placeholder="0"
+          value={tokenTwoAmount}
+          disabled={true}
+          // disabled={!prices}
+        />
+        <div className="assetOne">
+          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className="assetTwo">
+          <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
+
+          {tokenTwo.ticker}
+          <DownOutlined />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Swap
+export default Swap;
