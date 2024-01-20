b318603c77c85abcf35fd3b6860e80761b431ad8
Kandekore <darren@kandekore.net>
2024-01-20 00:03:02 +0000

    top 100 coins added

--- dex/src/components/Tokens.js
+++ dex/src/components/Tokens.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import CryptoCurrencyTable from "./CryptoCurrencyTable"
+
 function Tokens() {
   return (
     <div>Tokens</div>
+    <CryptoCurrencyTable/>
   )
 }
 
 export default Tokens
+
